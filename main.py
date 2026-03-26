189c4a2216afc04690d046dea569d48d252e02b9
nhogg <nathanhogg1223@gmail.com>
2026-03-25 22:21:40 -0500

    compute dtw path using numpy

--- main.py
+++ main.py
@@ -57,3 +57,51 @@ def compute_dtw_matrix(X, Y):
 
     return D_matrix[-1, -1], D_matrix
 
+def compute_dtw_path(D_matrix):
+    """
+    Trace back the optimal DTW path through the accum. cost matrix.
+
+    Args:
+        D_matrix: np.ndarray of shape (N, M) containing accumulated costs.
+
+    Returns:
+        np.ndarray of shape (K, 2) representing the warping path coords.
+    """
+    N, M = D_matrix.shape
+    i, j = N - 1, M - 1
+
+    path = [(i, j)]
+
+    # Walk backwards until we hit the starting corner (0, 0)
+    while i > 0 or j > 0:
+        if i == 0:
+            # Top edge reached, can only move left
+            j -= 1
+        elif j == 0:
+            # Left edge reached, can only move up
+            i -= 1
+        else:
+            # Look at three valid previous steps
+            cost_up = D_matrix[i - 1, j]
+            cost_left = D_matrix[i, j - 1]
+            cost_diag = D_matrix[i - 1, j - 1]
+
+            # Find cheapest path
+            min_cost = min(cost_p, cost_left, cost_diag)
+
+            if min_cost == cost_diag:
+                i -= 1
+                j -= 1
+            elif min_cost == cost_up:
+                i -= 1
+            else:
+                j -= 1
+
+        path.append((i, j))
+
+    # Path was built backwards, reverse
+    path.reverse()
+
+    return np.array(path)
+
+
